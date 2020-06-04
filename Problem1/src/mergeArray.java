c37d9bc8fc3a7795db800b0f5cd594db88917474
chetn <chetn@LAPTOP-DE7C8GNU>
2020-06-04 09:58:07 +0530

    commit

--- Problem1/src/mergeArray.java
+++ Problem1/src/mergeArray.java
@@ -1,47 +1,33 @@
-import java.util.stream.Stream;   
-import java.util.Arrays;   
-import java.io.*;  
-public class mergeArray {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.*;
+public class mergeArray  
+{  
+	public static void main(String[] args)   
+	{  
+		int[] firstArray = {23,45,5,78,4,90,1};        //source array  
+		int[] secondArray = {77,5,45,88,32,56,3};  //destination array  
+		int fal = firstArray.length;        //determines length of firstArray  
+		int sal = secondArray.length;   //determines length of secondArray  
+		int[] result = new int[fal + sal];  //resultant array of size first array and second array 
 
-	public static <T> Object[] mergeArray(T[] arr1, T[] arr2)   
-	{   
-	return Stream.of(arr1, arr2).flatMap(Stream::of).toArray();   
-	}  
+		System.arraycopy(firstArray, 0, result, 0, fal);  
+		System.arraycopy(secondArray, 0, result, fal, sal);
+		System.out.println(Arrays.toString(result));
+
+		int length = result.length;
+		int count = (int) Arrays.stream(result).filter(x -> x == 5).count();//count with 5
 	
-	static void pushfiveToEnd(Integer[] intArray, int n) 
-    { 
-        int count = 0;  // Count of non-zero elements 
-  
-        // Traverse the array. If element encountered is non-zero, then replace the element at index 'count' 
-        // with this element 
-        
-        for (int i = 0; i < n; i++) 
-            if (intArray[i] != 5) 
-                intArray[count++] = intArray[i]; 
-  
-        // Now all non-zero elements have been shifted to 
-        // front and 'count' is set as index of first 0. 
-        // Make all elements 0 from count to end. 
-        while (count < n) 
-            intArray[count++] = 5; 
-    } 
+		IntStream finalAraay =Arrays.stream(result).filter(x -> x != 5);
+		List<Integer> ints = finalAraay.mapToObj(Integer::valueOf).collect(Collectors.toList());
+		System.out.println(ints);
 	
-	public static void main(String[] args) {
-
-		Integer[] sourceArray = new Integer[]{2,3,4,5,7,5,8,9,5}; //source   
-		Integer[] destinationArray = new Integer[]{1,5,6}; //destination   
-		Object[] mergedArray = mergeArray(sourceArray,destinationArray); //merged   
-		
-		System.out.println("Merged array: "+ Arrays.toString(mergedArray)); 
-
-		Integer[] intArray =  Arrays.asList(mergedArray).toArray(new Integer[0]);
-		
-		int size = intArray.length;
-		
-		//code to push all 5 at end 	
-		pushfiveToEnd(intArray, size); 
-        System.out.println("Array after pushing 5 to the end: "); 
-        for (int i=0; i<size; i++) 
-            System.out.print(intArray[i]+" ");	
-	}
-}
+		while (count  > 0) 
+		{
+  
+			ints.add(5);
+			count -- ;
+		}
+	System.out.println("output is" + ints);
+  }  
+}  
